6ba7d663a083b9e655b4a40190e04b56106be6e3
Martin <133987180+martinthetech@users.noreply.github.com>
2024-11-12 17:02:06 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const addButton = document.querySelector('#add');
+const inputTask = document.querySelector('#task');
+const alert = document.querySelector('#alert');
+const taskList = document.getElementById("task-list");
+
+function addElement() {
+    if(inputTask.value.trim() !== "") {
+        const newTask = document.createElement("li");
+        newTask.textContent = inputTask.value;
+        newTask.innerHTML = start=1;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Remove";
+        deleteButton.className = "btn btn-danger ml-2";
+
+        deleteButton.addEventListener("click", () => {
+            taskList.removeChild(newTask);
+        });
+
+        newTask.appendChild(deleteButton);
+        taskList.appendChild(newTask);  
+        inputTask.value = "";
+        alert.className = "";
+        alert.textContent = "";
+    } else {
+        alert.textContent = "Please Enter your task at first!";
+        alert.className = "alert alert-warning alert-dismissible fade show";
+        setTimeout(() => {
+            alert.classList.add('fade-out');
+        }, 800);
+
+        setTimeout(() => {
+            alert.textContent = "";
+            alert.className = "";
+        }, 800);
+    }
+}
+
+addButton.addEventListener("click", addElement);
